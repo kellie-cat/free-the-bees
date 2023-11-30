a3dc82ddccf82053e4eb8cca4f39d5635ae7fc5c
kellie-cat <>
2023-11-30 20:41:12 +0000

    Check functions

--- run.py
+++ run.py
@@ -2,7 +2,29 @@ from random import randint
 
 scores = {'computer': 0, 'player': 0}
 
-  
+
+class Hive:
+    """
+    The game is played on a board representing a hive. This class sets the
+    board size, the number of bees, the player's names and the hive type
+    """
+
+    def __init__(self, size, num_bees, name, type):
+        self.size = size
+        self.hive = [["." for x in range(size)] for y in range(size)]
+        self.num_bees = num_bees
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.bees = []
+
+    def guess():
+        print(guess)
+
+    def add_bee():
+        print(add_bee)
+
+
 def random_point():
     """
     Helper function to return random integer that fits in the hive
@@ -24,6 +46,15 @@ def play_game():
 
 def new_game():
     print('new_game')
+
+
+random_point()
+
+
+validate_guess()
+
+
+place_bees()
 
 
 play_game()
