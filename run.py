c145944bed033b7aaf2043e2c13ef8bbf1cfbc22
kellie-cat <>
2023-11-30 20:35:43 +0000

    Outline function structure

--- run.py
+++ run.py
@@ -1,1 +1,32 @@
+from random import randint
 
+scores = {'computer': 0, 'player': 0}
+
+  
+def random_point():
+    """
+    Helper function to return random integer that fits in the hive
+    """
+    print('random_point')
+
+
+def validate_guess():
+    print('validate_guess')
+
+
+def place_bees():
+    print('place_bees')
+
+
+def play_game():
+    print('play_game')
+
+
+def new_game():
+    print('new_game')
+
+
+play_game()
+
+
+new_game()
